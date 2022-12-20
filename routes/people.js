824b23c2a18c1890ebf1a38a300273f962e9fe82
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-12-20 18:54:52 +0100

    added controllers && routers and added conrollers

--- routes/people.js
+++ routes/people.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+
 const { getPeople, createPerson,putPeople, deletePeople } = require('../controllers/people')
-router.get('/',getPeople)
-router.post('/',createPerson)
-router.put('/:id',putPeople)
-router.delete('/:id',deletePeople)
+
+
+router.route('/').get(getPeople).post(createPerson)
+router.route('/:id').put(putPeople).delete(deletePeople)
 
 module.exports = router
 
